9f12d07acc7171a5c86ee3728ff909740d058333
LouisWackenier <Louis.wackenier@gmail.com>
2024-02-10 14:08:45 +0100

    Payment process foundation set

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+
+function App() {
+
+  return (
+    <div>
+      <Navbar />
+      <h1>payment process</h1>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
