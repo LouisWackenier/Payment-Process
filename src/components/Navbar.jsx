5a57376ff4b7e5762bf9de3a3db72480da177f4c
LouisWackenier <Louis.wackenier@gmail.com>
2024-02-13 10:51:33 +0100

    Button styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,20 +11,49 @@ const MainContainer = styled.div`
 
 const Title = styled.h3`
    background-color: transparent;
+   margin-left: 1vw;
 `;
 
-const Buttons = styled.button`
-    text-align: center;
-    font-weight: 1rem;
+const DepositButton = styled.button`
+    border: transparent;
+    border-radius: 10px;
+    background-color: #1B7E7D;
+    font-size: 4rem;
+    margin-top: 10px;
+
+    width: 110px;
+    height: 110px;
+`;
+
+const RegistrationButton = styled.button`
+    background-color: #BDD922;
+    font-size: 3rem;
+    width: 5vw;
+
+    margin-left: 1vw;
+`;
+
+const LogInButton = styled.button`
+    background-color: #197272;
+    font-size: 3rem;
+    width: 4vw;
+`;
+
+const ButtonContainer = styled.div`
+    display: flex;
+    text-align: right;
+    margin-left: 75vw;
 `;
 
 const Navbar = () => {
   return (
     <MainContainer>
          <Title>Title Site</Title>     
-         <Buttons>$</Buttons>   
-         <Buttons>Registration</Buttons>   
-         <Buttons>Log In</Buttons>   
+         <ButtonContainer>
+             <DepositButton>$</DepositButton>   
+             <RegistrationButton>Registration</RegistrationButton>   
+             <LogInButton>Log In</LogInButton>   
+         </ButtonContainer>
     </MainContainer>
   )
 }
