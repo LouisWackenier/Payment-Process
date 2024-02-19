142386f49c255bb43d081f751c634d1a6506f498
LouisWackenier <Louis.wackenier@gmail.com>
2024-02-19 06:55:10 +0100

    Built the containers for the payment info

--- src/Pages/SelectPay.jsx
+++ src/Pages/SelectPay.jsx
@@ -1,3 +1,15 @@
+import styled from "styled-components"
+
+const MainContainer = styled.div`
+      background-color: #0D4D4D;
+`;
+
+const MenuList = styled.div`
+
+`;
+
+const PaymentListContainer = styled.div`
+`;
 
 const SelectPay = () => {
   return (
